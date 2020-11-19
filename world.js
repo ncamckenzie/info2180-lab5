361cfe81f53bb7444f64b19492f4e1482b1e29cb
Nicholas McKenzie <ncamckenzie@gmail.com>
2020-11-19 15:41:07 -0500

    Exercise 5 Completed.

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     var lookupbtn = document.getElementById("lookup");
+    var citiesbtn = document.getElementById("cities");
     lookupbtn.addEventListener('click',function(e){
         e.preventDefault();
         var lookupinput = document.getElementById("country").value;
@@ -13,7 +14,23 @@ window.onload = function(){
             document.getElementById("result").innerHTML = this.responseText;
           }
         }
-        httpRequest.open("GET","http://localhost/info2180-lab5/world.php?country=" + lookupinput,true);
+        httpRequest.open("GET","http://localhost/info2180-lab5/world.php?country=" + lookupinput + "&context=countries", true);
         httpRequest.send();
     }
-)}
+    
+); 
+  citiesbtn.addEventListener('click',function(e){
+  e.preventDefault();
+  var lookupinput = document.getElementById("country").value;
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200 && lookupinput =='') {
+          document.getElementById("result").innerHTML = this.responseText;
+      
+    }
+    if (this.readyState == 4 && this.status == 200 && lookupinput !=''){
+      document.getElementById("result").innerHTML = this.responseText;
+    }
+  }
+  httpRequest.open("GET","http://localhost/info2180-lab5/world.php?country=" + lookupinput + "&context=cities",true);
+  httpRequest.send();})}
--- world.php
+++ world.php
@@ -8,13 +8,31 @@ $dbname = 'world';
 $country=$_GET['country'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
+if ($_GET['context'] == "cities"){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM `cities` JOIN `countries` ON cities.country_code=countries.code WHERE countries.name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+  <table>
+  <tr>
+    <th id="name">Name</th>
+    <th id="district">District</th>
+    <th id="population">Population</th>
+</tr>
+<?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name']; ?> </td>
+      <td><?= $row['district']; ?> </td>
+      <td><?= $row['population']; ?> </td>
+</tr> 
+<?php endforeach; ?>
+</table>
+<?php } ?>
+<?php
+if ($_GET['context'] == "countries") {$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'"); 
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+  
 
-?>
 <table>
   <tr>
     <th id="name">Name</th>
@@ -31,3 +49,4 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </tr>
 <?php endforeach; ?>
 </table>
+<?php }?>
